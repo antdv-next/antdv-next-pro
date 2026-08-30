073c832a790d077a07b373caa8259a0d7b064e30
Bin <fuwb0407@gmail.com>
2026-08-30 22:23:13 +0800

    test(pro): stabilize Heatmap demo snapshots

--- packages/pro/src/heatmap/tests/demo.test.ts
+++ packages/pro/src/heatmap/tests/demo.test.ts
@@ -1,3 +1,13 @@
+import { afterAll, beforeAll } from 'vitest'
+import { resetMockDate, setMockDate } from '../../../../../tests/utils'
 import demoTest from '/@tests/shared/demoTest'
+
+beforeAll(() => {
+  setMockDate('2024-08-30T12:00:00.000Z')
+})
+
+afterAll(() => {
+  resetMockDate()
+})
 
 demoTest('heatmap')
