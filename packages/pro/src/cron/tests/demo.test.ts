9d5e117289084dfd41c2c1f4a31ea4bb684adc96
Bin <fuwb0407@gmail.com>
2026-09-08 22:40:13 +0800

    test(cron): pin demo timezone

--- packages/pro/src/cron/tests/demo.test.ts
+++ packages/pro/src/cron/tests/demo.test.ts
@@ -2,6 +2,15 @@ import MockDate from 'mockdate'
 import { afterAll } from 'vitest'
 import demoTest from '/@tests/shared/demoTest'
 
+const originalTimeZone = process.env.TZ
+
+process.env.TZ = 'Asia/Shanghai'
 MockDate.set('2026-09-08T00:00:00+08:00')
-afterAll(() => MockDate.reset())
+afterAll(() => {
+  MockDate.reset()
+  if (originalTimeZone === undefined)
+    delete process.env.TZ
+  else
+    process.env.TZ = originalTimeZone
+})
 demoTest('cron')
